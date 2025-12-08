9e84614e3433544b692335cb44d815ffab3cee7c
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-08 17:01:07 +0530

    Solved number of digits of a number

---
+++ Basics/numberOfDigits.js
@@ -0,0 +1,16 @@
+//Write a function that returns the count of digits in a number.
+function countDigits(n) {
+  if (n === 0) {
+    return 1;
+  }
+  n = Math.abs(n);
+  let count = 0;
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count++;
+  }
+  return count;
+}
+let num = 34560;
+let result = countDigits(num);
+console.log(result);
---
+++ Basics/patterns.js
@@ -0,0 +1,45 @@
+//Square pattern
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = " ";
+//   for (let j = 1; j <= n; j++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = " ";
+//   for (let j = 1; j <= i; j++) {
+//     row = row + "* ";
+//   }
+//   console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = " ";
+//   for (let j = 1; j <= i; j++) {
+//     row += j + " ";
+//   }
+//   console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = " ";
+//   for (let j = 1; j <= i; j++) {
+//     row += i + " ";
+//   }
+//   console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = " ";
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     row = row + j + " ";
+//   }
+//   console.log(row);
+// }

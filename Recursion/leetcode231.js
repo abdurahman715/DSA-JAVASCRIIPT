8eed6021059ab9a153a945586b9d4231ac41204d
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-15 12:14:55 +0530

    Soved Leetcode Power of two problem

---
+++ Recursion/leetcode231.js
@@ -0,0 +1,10 @@
+//✅Leetcode-231 Power of Two
+function isPowerOfTwo(n) {
+  if (n === 1) {
+    return true;
+  } else if (n % 2 !== 0 || n < 1) {
+    return false;
+  }
+  return isPowerOfTwo(n / 2);
+}
+console.log(isPowerOfTwo(16));

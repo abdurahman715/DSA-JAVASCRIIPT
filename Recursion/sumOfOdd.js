ca127621467832475b0b5559c9dcbab40c3029f2
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-15 11:24:44 +0530

    Solved Sum of all odd numbers problem using recursion

---
+++ Recursion/sumOfOdd.js
@@ -0,0 +1,10 @@
+//Sum of all odd numbers in an array
+let arr = [5, 3, 2, 0, 1, 6];
+function sum(n) {
+  isOdd = arr[n] % 2 !== 0;
+  if (n == 0) {
+    return isOdd ? arr[n] : 0;
+  }
+  return (isOdd ? arr[n] : 0) + sum(n - 1);
+}
+console.log(sum(arr.length - 1));

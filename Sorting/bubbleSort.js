d42d7fae5ab9427b42813edce473f8db590ba7f5
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-17 07:52:29 +0530

    Learned Bubble Sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+let arr = [5, 4, 9, 1, 0];
+function bubbleSort(nums) {
+  let n = nums.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        //Swapping
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+  return nums;
+}
+console.log(bubbleSort(arr));
